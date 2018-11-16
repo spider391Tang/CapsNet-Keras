2ba0af062c50fa7170945e91d00f568d76c07b6b
TangQuincy <quincy.tang@garmin.com>
2018-11-16 22:37:54 +0800

    spider391 modify for CRNN

--- capsulenet.py
+++ capsulenet.py
@@ -16,17 +16,132 @@ Result:
 Author: Xifeng Guo, E-mail: `guoxifeng1990@163.com`, Github: `https://github.com/XifengGuo/CapsNet-Keras`
 """
 
+import time
+import wave
+import librosa
 import numpy as np
 from keras import layers, models, optimizers
 from keras import backend as K
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
-from utils import combine_images
+from utils import combine_images, create_folder, split_in_seqs, split_multi_channels
 from PIL import Image
 from capsulelayers import CapsuleLayer, PrimaryCap, Length, Mask
+from sklearn import preprocessing
 
 K.set_image_data_format('channels_last')
 
+
+def preprocess_data(_X, _Y, _X_test, _Y_test, _seq_len, _nb_ch):
+    # split into sequences
+    _X = split_in_seqs(_X, _seq_len)
+    _Y = split_in_seqs(_Y, _seq_len)
+
+    _X_test = split_in_seqs(_X_test, _seq_len)
+    _Y_test = split_in_seqs(_Y_test, _seq_len)
+
+    _X = split_multi_channels(_X, _nb_ch)
+    _X_test = split_multi_channels(_X_test, _nb_ch)
+    return _X, _Y, _X_test, _Y_test
+
+def load_data(_feat_folder, _mono, _fold=None):
+    feat_file_fold = os.path.join(_feat_folder, 'mbe_{}_fold{}.npz'.format('mon' if _mono else 'bin', _fold))
+    dmp = np.load(feat_file_fold)
+    _X_train, _Y_train, _X_test, _Y_test = dmp['arr_0'],  dmp['arr_1'],  dmp['arr_2'],  dmp['arr_3']
+    return _X_train, _Y_train, _X_test, _Y_test
+
+def load_audio(filename, mono=True, fs=44100):
+    """Load audio file into numpy array
+    Supports 24-bit wav-format
+    
+    Taken from TUT-SED system: https://github.com/TUT-ARG/DCASE2016-baseline-system-python
+    
+    Parameters
+    ----------
+    filename:  str
+        Path to audio file
+
+    mono : bool
+        In case of multi-channel audio, channels are averaged into single channel.
+        (Default value=True)
+
+    fs : int > 0 [scalar]
+        Target sample rate, if input audio does not fulfil this, audio is resampled.
+        (Default value=44100)
+
+    Returns
+    -------
+    audio_data : numpy.ndarray [shape=(signal_length, channel)]
+        Audio
+
+    sample_rate : integer
+        Sample rate
+
+    """
+
+    file_base, file_extension = os.path.splitext(filename)
+    if file_extension == '.wav':
+        _audio_file = wave.open(filename)
+
+        # Audio info
+        sample_rate = _audio_file.getframerate()
+        sample_width = _audio_file.getsampwidth()
+        number_of_channels = _audio_file.getnchannels()
+        number_of_frames = _audio_file.getnframes()
+
+        # Read raw bytes
+        data = _audio_file.readframes(number_of_frames)
+        _audio_file.close()
+
+        # Convert bytes based on sample_width
+        num_samples, remainder = divmod(len(data), sample_width * number_of_channels)
+        if remainder > 0:
+            raise ValueError('The length of data is not a multiple of sample size * number of channels.')
+        if sample_width > 4:
+            raise ValueError('Sample size cannot be bigger than 4 bytes.')
+
+        if sample_width == 3:
+            # 24 bit audio
+            a = np.empty((num_samples, number_of_channels, 4), dtype=np.uint8)
+            raw_bytes = np.fromstring(data, dtype=np.uint8)
+            a[:, :, :sample_width] = raw_bytes.reshape(-1, number_of_channels, sample_width)
+            a[:, :, sample_width:] = (a[:, :, sample_width - 1:sample_width] >> 7) * 255
+            audio_data = a.view('<i4').reshape(a.shape[:-1]).T
+        else:
+            # 8 bit samples are stored as unsigned ints; others as signed ints.
+            dt_char = 'u' if sample_width == 1 else 'i'
+            a = np.fromstring(data, dtype='<%s%d' % (dt_char, sample_width))
+            audio_data = a.reshape(-1, number_of_channels).T
+
+        if mono:
+            # Down-mix audio
+            audio_data = np.mean(audio_data, axis=0)
+
+        # Convert int values into float
+        audio_data = audio_data / float(2 ** (sample_width * 8 - 1) + 1)
+
+        # Resample
+        if fs != sample_rate:
+            audio_data = librosa.core.resample(audio_data, sample_rate, fs)
+            sample_rate = fs
+
+        return audio_data, sample_rate
+    return None, None
+
+def load_desc_file(_desc_file):
+    _desc_dict = dict()
+    for line in open(_desc_file):
+        words = line.strip().split('\t')
+        name = words[0].split('/')[-1]
+        if name not in _desc_dict:
+            _desc_dict[name] = list()
+        _desc_dict[name].append([float(words[2]), float(words[3]), __class_labels[words[-1]]])
+    return _desc_dict
+
+def extract_mbe(_y, _sr, _nfft, _nb_mel):
+    spec, n_fft = librosa.core.spectrum._spectrogram(y=_y, n_fft=_nfft, hop_length=_nfft/2, power=1)
+    mel_basis = librosa.filters.mel(sr=_sr, n_fft=_nfft, n_mels=_nb_mel)
+    return np.log(np.dot(mel_basis, spec))
 
 def CapsNet(input_shape, n_class, routings):
     """
@@ -40,10 +155,10 @@ def CapsNet(input_shape, n_class, routings):
     x = layers.Input(shape=input_shape)
 
     # Layer 1: Just a conventional Conv2D layer
-    conv1 = layers.Conv2D(filters=256, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
+    conv1 = layers.Conv2D(filters=256, kernel_size=(3, 3), strides=1, padding='same', activation='relu', name='conv1')(x)
 
     # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
-    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=9, strides=2, padding='valid')
+    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=(3, 3), strides=2, padding='same')
 
     # Layer 3: Capsule layer. Routing algorithm works here.
     digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16, routings=routings,
@@ -192,10 +307,15 @@ def load_mnist():
     # the data, shuffled and split between train and test sets
     from keras.datasets import mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
+ 
+    print "load_mnist"
+    print x_train.shape
     x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    print x_train.shape
     x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    print y_train.shape
     y_train = to_categorical(y_train.astype('float32'))
+    print y_train.shape
     y_test = to_categorical(y_test.astype('float32'))
     return (x_train, y_train), (x_test, y_test)
 
@@ -235,10 +355,89 @@ if __name__ == "__main__":
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
 
+    # ###################################################################
+    #              Main script starts here
+    # ###################################################################
+
+    is_mono = False
+    __class_labels = {
+        'brakes squeaking': 0,
+        'car': 1,
+        'children': 2,
+        'large vehicle': 3,
+        'people speaking': 4,
+        'people walking': 5
+    }
+
+    # location of data.
+    folds_list = [1] #[1, 2, 3, 4]
+    evaluation_setup_folder = '/Users/spider391tang/projects/TUT/TUT-sound-events-2017-development/evaluation_setup'
+    audio_folder = '/Users/spider391tang/projects/TUT/TUT-sound-events-2017-development/audio/street'
+    # audio_folder = '/Users/spider391tang/projects/learn_feature'
+
+    # Output
+    feat_folder = '/Users/spider391tang/projects/TUT/feat/'
+    create_folder(feat_folder)
+
+    #######################################################################################
+    # MAIN SCRIPT STARTS HERE
+    #######################################################################################
+
+    __fig_name = '{}_{}'.format('mon' if is_mono else 'bin', time.strftime("%Y_%m_%d_%H_%M_%S"))
+
+    nb_ch = 1 if is_mono else 2
+    batch_size = 128    # Decrease this if you want to run on smaller GPU's
+    seq_len = 256       # Frame sequence length. Input to the CRNN.
+    nb_epoch = 500      # Training epochs
+    patience = int(0.25 * nb_epoch)  # Patience for early stopping
+
+    # Number of frames in 1 second, required to calculate F and ER for 1 sec segments.
+    # Make sure the nfft and sr are the same as in feature.py
+    sr = 44100
+    nfft = 2048
+    frames_1_sec = int(sr/(nfft/2.0))
+
+    print('\n\nUNIQUE ID: {}'.format(__fig_name))
+    print('TRAINING PARAMETERS: nb_ch: {}, seq_len: {}, batch_size: {}, nb_epoch: {}, frames_1_sec: {}'.format(
+        nb_ch, seq_len, batch_size, nb_epoch, frames_1_sec))
+
+    # Folder for saving model and training curves
+    __models_dir = 'models/'
+    create_folder(__models_dir)
+
+    # CRNN model definition
+    cnn_nb_filt = 128            # CNN filter size
+    cnn_pool_size = [5, 2, 2]   # Maxpooling across frequency. Length of cnn_pool_size =  number of CNN layers
+    rnn_nb = [32, 32]           # Number of RNN nodes.  Length of rnn_nb =  number of RNN layers
+    fc_nb = [32]                # Number of FC nodes.  Length of fc_nb =  number of FC layers
+    dropout_rate = 0.5          # Dropout after each layer
+    #print('MODEL PARAMETERS:\n cnn_nb_filt: {}, cnn_pool_size: {}, rnn_nb: {}, fc_nb: {}, dropout_rate: {}'.format( cnn_nb_filt, cnn_pool_size, rnn_nb, fc_nb, dropout_rate))
+
+    avg_er = list()
+    avg_f1 = list()
+
+    # User set parameters
+    win_len = nfft
+    hop_len = win_len / 2
+    nb_mel_bands = 40
+
+    X, Y, X_test, Y_test = load_data(feat_folder, is_mono, 1)
+    print X.shape
+    print Y.shape
+    print "number of channel: ", nb_ch 
+    print "seq_len : ", seq_len
+
+    x_train, y_train, x_test, y_test = preprocess_data(X, Y, X_test, Y_test, seq_len, nb_ch)
+
+    print x_train.shape
+    print y_train.shape
     # load data
     (x_train, y_train), (x_test, y_test) = load_mnist()
+    # print x_train.shape
+    # print y_train.shape
 
     # define model
+    # print x_train
     model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
                                                   n_class=len(np.unique(np.argmax(y_train, 1))),
                                                   routings=args.routings)
