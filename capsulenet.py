728c740347a0bef87f8f696ece6fa7ab2850f7ac
yhtseng <yhtseng@gmail.com>
2018-12-25 15:29:51 +0800

    Multi-channel Support

--- capsulenet.py
+++ capsulenet.py
@@ -51,6 +51,8 @@ def f1(y_true, y_pred):
 
 def preprocess_data(_X, _Y, _X_test, _Y_test, _seq_len, _nb_ch):
     # split into sequences
+
+    print "before split multi channel: ", _X.shape
     _X = split_in_seqs(_X, 1)
     #_Y = split_in_seqs(_Y, 1)
 
@@ -60,8 +62,8 @@ def preprocess_data(_X, _Y, _X_test, _Y_test, _seq_len, _nb_ch):
     print "split multi channel: ", _X.shape
     _X = split_multi_channels(_X, _nb_ch)
     _X_test = split_multi_channels(_X_test, _nb_ch)
-    _X = _X.reshape(-1, 1, 40, 1).astype('float32')
-    _X_test = _X_test.reshape(-1, 1, 40, 1).astype('float32')
+    # _X = _X.reshape(-1, 1, 40, 2).astype('float32')
+    # _X_test = _X_test.reshape(-1, 1, 40, 2).astype('float32')
 
     # add extra label
 
@@ -90,99 +92,6 @@ def load_data(_feat_folder, _mono, _fold=None):
     _X_train, _Y_train, _X_test, _Y_test = dmp['arr_0'],  dmp['arr_1'],  dmp['arr_2'],  dmp['arr_3']
     return _X_train, _Y_train, _X_test, _Y_test
 
-def load_audio(filename, mono=True, fs=44100):
-    """Load audio file into numpy array
-    Supports 24-bit wav-format
-    
-    Taken from TUT-SED system: https://github.com/TUT-ARG/DCASE2016-baseline-system-python
-    
-    Parameters
-    ----------
-    filename:  str
-        Path to audio file
-
-    mono : bool
-        In case of multi-channel audio, channels are averaged into single channel.
-        (Default value=True)
-
-    fs : int > 0 [scalar]
-        Target sample rate, if input audio does not fulfil this, audio is resampled.
-        (Default value=44100)
-
-    Returns
-    -------
-    audio_data : numpy.ndarray [shape=(signal_length, channel)]
-        Audio
-
-    sample_rate : integer
-        Sample rate
-
-    """
-
-    file_base, file_extension = os.path.splitext(filename)
-    if file_extension == '.wav':
-        _audio_file = wave.open(filename)
-
-        # Audio info
-        sample_rate = _audio_file.getframerate()
-        sample_width = _audio_file.getsampwidth()
-        number_of_channels = _audio_file.getnchannels()
-        number_of_frames = _audio_file.getnframes()
-
-        # Read raw bytes
-        data = _audio_file.readframes(number_of_frames)
-        _audio_file.close()
-
-        # Convert bytes based on sample_width
-        num_samples, remainder = divmod(len(data), sample_width * number_of_channels)
-        if remainder > 0:
-            raise ValueError('The length of data is not a multiple of sample size * number of channels.')
-        if sample_width > 4:
-            raise ValueError('Sample size cannot be bigger than 4 bytes.')
-
-        if sample_width == 3:
-            # 24 bit audio
-            a = np.empty((num_samples, number_of_channels, 4), dtype=np.uint8)
-            raw_bytes = np.fromstring(data, dtype=np.uint8)
-            a[:, :, :sample_width] = raw_bytes.reshape(-1, number_of_channels, sample_width)
-            a[:, :, sample_width:] = (a[:, :, sample_width - 1:sample_width] >> 7) * 255
-            audio_data = a.view('<i4').reshape(a.shape[:-1]).T
-        else:
-            # 8 bit samples are stored as unsigned ints; others as signed ints.
-            dt_char = 'u' if sample_width == 1 else 'i'
-            a = np.fromstring(data, dtype='<%s%d' % (dt_char, sample_width))
-            audio_data = a.reshape(-1, number_of_channels).T
-
-        if mono:
-            # Down-mix audio
-            audio_data = np.mean(audio_data, axis=0)
-
-        # Convert int values into float
-        audio_data = audio_data / float(2 ** (sample_width * 8 - 1) + 1)
-
-        # Resample
-        if fs != sample_rate:
-            audio_data = librosa.core.resample(audio_data, sample_rate, fs)
-            sample_rate = fs
-
-        return audio_data, sample_rate
-    return None, None
-
-def load_desc_file(_desc_file):
-    _desc_dict = dict()
-    for line in open(_desc_file):
-        words = line.strip().split('\t')
-        name = words[0].split('/')[-1]
-        if name not in _desc_dict:
-            _desc_dict[name] = list()
-        _desc_dict[name].append([float(words[2]), float(words[3]), __class_labels[words[-1]]])
-    return _desc_dict
-
-def extract_mbe(_y, _sr, _nfft, _nb_mel):
-    spec, n_fft = librosa.core.spectrum._spectrogram(y=_y, n_fft=_nfft, hop_length=_nfft/2, power=1)
-    mel_basis = librosa.filters.mel(sr=_sr, n_fft=_nfft, n_mels=_nb_mel)
-    return np.log(np.dot(mel_basis, spec))
-
 def CapsNet(input_shape, n_class, routings):
     """
     A Capsule Network on MNIST.
@@ -195,11 +104,11 @@ def CapsNet(input_shape, n_class, routings):
     x = layers.Input(shape=input_shape)
 
     # Layer 1: Just a conventional Conv2D layer
-    conv1 = layers.Conv2D(filters=256, kernel_size=(3, 9), strides=1, padding='same', activation='relu', name='conv1')(x)
+    conv1 = layers.Conv2D(filters=256, kernel_size=(5, 5), strides=1, padding='same', activation='relu', name='conv1')(x)
     #conv1 = layers.Conv2D(filters=256, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
 
     # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
-    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=(3, 9), strides=2, padding='same')
+    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=(5, 5), strides=2, padding='same')
     #primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=9, strides=2, padding='valid')
 
     # Layer 3: Capsule layer. Routing algorithm works here.
@@ -452,7 +361,7 @@ if __name__ == "__main__":
     #              Main script starts here
     # ###################################################################
 
-    is_mono = True
+    is_mono = False
     __class_labels = {
         'brakes squeaking': 0,
         'car': 1,
@@ -464,12 +373,8 @@ if __name__ == "__main__":
 
     # location of data.
     folds_list = [1] #[1, 2, 3, 4]
-    # evaluation_setup_folder = '/Users/spider391tang/projects/TUT/TUT-sound-events-2017-development/evaluation_setup'
-    # audio_folder = '/Users/spider391tang/projects/TUT/TUT-sound-events-2017-development/audio/street'
-    # audio_folder = '/Users/spider391tang/projects/learn_feature'
 
     # Output
-    # feat_folder = '/Users/spider391tang/projects/TUT/feat/'
     feat_folder = '/home/yhtseng/projects/data/feat/'
     create_folder(feat_folder)
 
